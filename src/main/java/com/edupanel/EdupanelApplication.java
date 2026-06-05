ca734b12239d61ec48657655da09ce98cd43912e
iShadowOnce <j.astudillo06@ufromail.cl>
2026-06-05 02:38:55 -0400

    Inicio del proyecto Spring Boot

---
+++ src/main/java/com/edupanel/EdupanelApplication.java
@@ -0,0 +1,13 @@
+package com.edupanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdupanelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdupanelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edupanel/EdupanelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edupanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdupanelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
