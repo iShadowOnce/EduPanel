593f32fd9a733d8cad623225a1d383fd365a3c38
Tobal Herrera <crisherreragramunt@gmail.com>
2026-06-24 00:32:48 -0400

    Nueva clase FirebaseConfig con el metodo inicializar para hacer funcionr firebase

---
+++ src/main/java/com/edupanel/config/FirebaseConfig.java
@@ -0,0 +1,36 @@
+package com.edupanel.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class FirebaseConfig {
+    public static void inicializar() {
+        try {
+            if (!FirebaseApp.getApps().isEmpty()) {
+                System.out.println("Firebase ya estaba inicializado");
+                return;
+            }
+
+            InputStream flujoCuentaServicio  = FirebaseConfig.class
+                    .getClassLoader()
+                    .getResourceAsStream("firebase-config.json");
+
+            FirebaseOptions opcion  = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(flujoCuentaServicio ))
+                    .setDatabaseUrl("https://poocalificaciones-default-rtdb.firebaseio.com")
+                    .build();
+
+            FirebaseApp.initializeApp(opcion );
+            System.out.println("Firebase inicializado correctamente.");
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error al inicializar Firebase: " + e.getMessage(), e);
+        }
+    }
+}
+
+
