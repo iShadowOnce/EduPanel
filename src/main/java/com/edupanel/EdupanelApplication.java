2968033cad62807b8a90bb23378dda585da3d1ee
Tobal Herrera <crisherreragramunt@gmail.com>
2026-06-25 23:44:43 -0400

    nota app: recordar perfil firebase

--- src/main/java/com/edupanel/EdupanelApplication.java
+++ src/main/java/com/edupanel/EdupanelApplication.java
@@ -9,5 +9,8 @@ public class EdupanelApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EdupanelApplication.class, args);
 	}
+	//acuerdate crear en arranque el perfil "firebase"
+	//si quieres usar el arranque a memoria usar cualquiera distinto de firebase
+	// ya que esta con "!firebase"
 
 }
